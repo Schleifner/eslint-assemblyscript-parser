e11e49f63f60c8c004d4421a322e5c464d63f538
KKiiim <rui.jin@partner.bmw.com>
2023-11-02 18:19:18 +0800

    update asParser

--- src/asParser.cjs
+++ src/asParser.cjs
@@ -1,9 +1,6 @@
 const tsParse = require("@typescript-eslint/parser");
 const Utils = require("../../@typescript-eslint/typescript-estree/dist/node-utils.js");
 
-function parse(code, options) {
-  Utils.nodeCanBeDecorated = ()=>{return true};
-  return tsParse.parse(code, options);
-}
+Utils.nodeCanBeDecorated = ()=>{return true};
 
-module.exports = { parse };
+module.exports = { ...tsParse };
