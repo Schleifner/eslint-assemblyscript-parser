49c4d7d0082d3319107bb1cbfc364f9e56379d14
KKiiim <rui.jin@partner.bmw.com>
2023-11-03 09:52:43 +0800

    add esbuild

--- src/asParser.cjs
+++ src/asParser.cjs
@@ -1,6 +1,8 @@
 const tsParse = require("@typescript-eslint/parser");
 const Utils = require("../node_modules/@typescript-eslint/typescript-estree/dist/node-utils.js");
 
-Utils.nodeCanBeDecorated = ()=>{return true};
+Utils.nodeCanBeDecorated = () => {
+  return true;
+};
 
 module.exports = { ...tsParse };
